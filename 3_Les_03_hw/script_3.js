07e7ed2be9b603b09cd801566cff23ec0d3e551b
v-shchenyatskiy <9137644477@mail.ru>
2020-02-26 17:12:05 +0700

    ДЗ №3 1 С помощью for вывел значения от 0 до 10 с подписью ноль-нечетн-четн 2 Вывел в консоль значения рядом с комментариями 3 С помощью forEach применил скидку 15% ко всем продуктам 4 С помощью filter и sort получил товары с фото и отсортировал их по цене 5 С помощью for вывел числа от 0 до 9 без использования тела цикла (с пустым телом) 6 Нарисовал в консоли горку в 20 рядов

---
+++ 3_Les_03_hw/script_3.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach( product => console.log(product.price * 0.85) );
---
+++ 3_Les_03_hw/script_4.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: []
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg"
+        ]
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+const photoFilterResult = products.filter(function(product) {
+    return (product.photos != undefined && product.photos.length > 0);
+});
+
+photoFilterResult.sort(function(a, b) {
+    return a.price - b.price;
+})
+
+console.log(photoFilterResult);
