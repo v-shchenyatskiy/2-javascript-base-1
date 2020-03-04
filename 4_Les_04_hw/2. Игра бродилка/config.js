5aefd90f6dcd23298e8ea61310bff8b2df8bd887
v-shchenyatskiy <9137644477@mail.ru>
2020-03-04 19:13:44 +0700

    ДЗ №5 1 Задание 1 - Сделал модальное (исчезающее-появляющееся) окно 2 Задание 2 - Сделал подмену картинки на текст в .product с заменой текста на кнопке и ее функции 3 Задание 3, 4 - не успел, доделаю и выложу

--- 4_Les_04_hw/2. Игра бродилка/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Объект с настройками игры.
- * @property {int} rowsCount Количество строк в карте.
- * @property {int} colsCount Количество колонок в карте.
- */
-const config = {
-    rowsCount: 10,
-    colsCount: 10,
-};
